b323c144c10b3fdf2ac3d57b8dff0cc33445fe7f
lmusarella <Tirofly123>
2023-10-07 20:47:27 +0200

    [PSC]: Fix binance api changes

--- bot-lib/common/constants/api.constants.js
+++ bot-lib/common/constants/api.constants.js
@@ -1,7 +1,8 @@
-const BINANCE_API_BNB_USDT_URL = "https://data.binance.com/api/v3/ticker/price?symbol=BNBUSDT";
-const BINANCE_API_CAKE_USDT_URL = "https://data.binance.com/api/v3/ticker/price?symbol=CAKEUSDT";
+const BINANCE_API_BNB_USDT_URL = "https://data-api.binance.vision/api/v3/ticker/price?symbol=BNBUSDT";
+const BINANCE_API_CAKE_USDT_URL = "https://data-api.binance.vision/api/v3/ticker/price?symbol=CAKEUSDT";
 
 module.exports = {
     BINANCE_API_BNB_USDT_URL,
     BINANCE_API_CAKE_USDT_URL
 };
+
